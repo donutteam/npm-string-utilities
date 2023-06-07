4300d566f9313626ea48848caf585b0d9dc39f7b
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-06-07 17:13:42 -0400

    Adding isLocalIpAddress function.

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ if (globalThis.crypto == undefined)
 }
 
 //
-// Exports
+// Functions
 //
 
 /**
@@ -41,14 +41,42 @@ export function chunkify(inputString : string, chunkLength : number) : string[]
 	return inputString.match(new RegExp(".{1," + chunkLength.toString() + "}", "gu"));
 }
 
-/** A list of characters available to generateRandomString. */
-const randomStringCharacters : string[] = 
+/** A list of regular expressions that match local IP ranges. */
+const localIpRanges : RegExp[] = 
 [
-	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-	"_", "-",
+	// 10.0.0.0 - 10.255.255.255
+	/^(::f{4}:)?10\.\d{1,3}\.\d{1,3}\.\d{1,3}/,
+
+	// 127.0.0.0 - 127.255.255.255
+	/^(::f{4}:)?127\.\d{1,3}\.\d{1,3}\.\d{1,3}/,
+
+	// 169.254.1.0 - 169.254.254.255
+	/^(::f{4}:)?169\.254\.([1-9]|1?\d\d|2[0-4]\d|25[0-4])\.\d{1,3}/,
+
+	// 172.16.0.0 - 172.31.255.255
+	/^(::f{4}:)?(172\.1[6-9]|172\.2\d|172\.3[0-1])\.\d{1,3}\.\d{1,3}/,
+
+	// 192.168.0.0 - 192.168.255.255
+	/^(::f{4}:)?192\.168\.\d{1,3}\.\d{1,3}/,
+
+	// fc00::/7
+	/^f[c-d][0-9a-f]{2}(::1$|:[0-9a-f]{1,4}){1,7}/,
+
+	// fe80::/10
+	/^fe[89ab][0-9a-f](::1$|:[0-9a-f]{1,4}){1,7}/,
 ];
+
+/**
+ * Checks if the given IP address is a local IP address.
+ * 
+ * @param ipAddress An IP address.
+ * @returns Whether the given IP address is a local IP address.
+ * @author Loren Goodwin
+ */
+export function isLocalIpAddress(ipAddress : string) : boolean
+{
+	return ipAddress === "::1" || ipAddress === "::1" || localIpRanges.some((range) => range.test(ipAddress));
+}
 
 /**
  * Pads the given string to a multiple of 16 bytes with null characters.
@@ -72,6 +100,15 @@ export function padNull(inputString : string, length = 16) : string
 
 	return inputString;
 }
+
+/** A list of characters available to generateRandomString. */
+const randomStringCharacters : string[] = 
+[
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"_", "-",
+];
 
 /**
  * Generates a cryptographically secure random string.
